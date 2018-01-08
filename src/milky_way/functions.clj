f86e698b53485326b41a2ec3e1e28013b5d83703
Christos Chatzifountas <gavlooth@gmail.com>
2018-01-08 07:23:53 +0200

    Rename functions

--- src/milky_way/functions.clj
+++ src/milky_way/functions.clj
@@ -1,21 +1,21 @@
 (ns milky-way.functions
   (:import [org.apache.commons.math3.analysis.function Log Tanh Tan Sin Cos]))
 
-(def functions [(Log.) (Tan.) (Tanh.) (Sin.) (Cos.)])
+(def functions  {:Log (Log.) :Tan (Tan.) :Tanh (Tanh.) :Sin (Sin.) :Cos (Cos.)})
 (defn log [x]
-  (.value (first functions) (double x)))
+  (.value (:Log functions) (double x)))
 
 (defn tan [x]
-  (.value (second functions) (double  x)))
+  (.value (:Tan functions) (double  x)))
 
 (defn tanh [x]
-  (.value (get functions 2) (double  x)))
+  (.value (:Tanh functions) (double  x)))
 
 (defn sin [x]
-  (.value (get functions 3) (double  x)))
+  (.value (:Sin functions) (double  x)))
 
 (defn cos [x]
-  (.value (get functions 4) (double  x)))
+  (.value (:Cos functions) (double  x)))
 
 
 (defn spiral-galaxy
