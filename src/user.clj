0965bd8903c3df5f773a5e4a67cd9458111a7d0b
Christos Chatzifountas <gavlooth@gmail.com>
2018-01-09 10:44:43 +0200

    :construction: add gaussian distribution

--- src/user.clj
+++ src/user.clj
@@ -30,6 +30,6 @@
   "stops all states defined by defstate, reloads modified source files, and restarts the states"
   []
   (stop)
-  (tn/refresh :after 'dev/go))
+  (tn/refresh :after 'user/go))
 
 (mount/in-clj-mode)
